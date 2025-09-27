eec9f6efe369cd21bbd2f5e1e84cfb06491971fe
Harris Jerome J <146384771+HarrisJerome@users.noreply.github.com>
2025-09-27 16:34:31 +0530

    Add files via upload

---
+++ Calci/script.js
@@ -0,0 +1,23 @@
+const display = document.getElementById("display");
+
+function appendValue(value) {
+  if (display.innerText === "0" && value !== ".") {
+    display.innerText = value;
+  } else {
+    display.innerText += value;
+  }
+}
+
+function clearDisplay() {
+  display.innerText = "0";
+}
+
+function calculate() {
+  try {
+    display.innerText = eval(
+      display.innerText.replace("÷", "/").replace("×", "*")
+    );
+  } catch {
+    display.innerText = "Error";
+  }
+}
